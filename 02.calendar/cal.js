7838a81b2191ffcea82875acf2f7ebb9695620bd
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-07 08:36:49 +0900

    使用頻度の少ない変数定義を削除

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,20 +1,16 @@
 import minimist from "minimist";
 
 const today = new Date();
-const thisYear = today.getFullYear();
-const thisMonth = today.getMonth() + 1;
-const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
-
 const args = minimist(process.argv.slice(2));
-const year = args["y"]? parseInt(args["y"], 10) : thisYear;
-const month = args["m"]? parseInt(args["m"], 10) : thisMonth;
+const year = args["y"]? parseInt(args["y"], 10) : today.getFullYear();
+const month = args["m"]? parseInt(args["m"], 10) : today.getMonth() + 1;
 
 const firstDay = new Date(year, month - 1, 1);
 const lastDay = new Date(year, month, 0);
 
 function outputCalendar(year, month) {
   console.log(`      ${month}月 ${year}`);
-  console.log(weekdays.join(" "));
+  console.log("日 月 火 水 木 金 土");
 
   const startingDay = firstDay.getDay();
   process.stdout.write(" ".repeat(startingDay * 3));
