85ae713d28ebe96623ccd42a28345f1652265571
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-10 08:59:02 +0900

    whileの使用とlintの修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -6,24 +6,20 @@ function outputCalendar(year, month) {
   const firstDate = new Date(year, month - 1, 1);
   const lastDate = new Date(year, month, 0);
   const firstWeekday = firstDate.getDay();
-
   console.log(`      ${month}月 ${year}`);
   console.log("日 月 火 水 木 金 土");
   process.stdout.write("   ".repeat(firstDate.getDay()));
-  for (
-    let date = firstDate;
-    date <= lastDate;
-    date.setDate(date.getDate() + 1)
-  ) {
-    process.stdout.write(date.getDate().toString().padStart(2));
+  while (firstDate <= lastDate) {
+    process.stdout.write(firstDate.getDate().toString().padStart(2));
     if (
-      (firstWeekday + date.getDate()) % 7 === 0 ||
-      date.getDate() === lastDate.getDate()
+      (firstWeekday + firstDate.getDate()) % 7 === 0 ||
+      firstDate.getDate() === lastDate.getDate()
     ) {
       console.log();
     } else {
       process.stdout.write(" ");
     }
+    firstDate.setDate(firstDate.getDate() + 1);
   }
 }
 
