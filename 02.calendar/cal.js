ebb6e2d175efa641c93052f28a24e1b3749552cc
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-09 21:43:41 +0900

    オブジェクトのまま処理を実行

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,14 +1,16 @@
 import minimist from "minimist";
 
 function outputCalendar(year, month) {
-  const firstWeekDay = new Date(year, month - 1, 1).getDay();
-  const lastDate = new Date(year, month, 0).getDate();
+  const firstDate = new Date(year, month - 1, 1);
+  const lastDate = new Date(year, month, 0);
+  const firstWeekday = firstDate.getDay();
+
   console.log(`      ${month}月 ${year}`);
   console.log("日 月 火 水 木 金 土");
-  process.stdout.write("   ".repeat(firstWeekDay));
-  for (let i = 1; i <= lastDate; i++) {
-    process.stdout.write(`${i.toString().padStart(2)}`);
-    if ((firstWeekDay + i) % 7 === 0 || i === lastDate) {
+  process.stdout.write("   ".repeat(firstDate.getDay()));
+  for (let date = firstDate; date <= lastDate; date.setDate(date.getDate() + 1)) {
+    process.stdout.write(date.getDate().toString().padStart(2));
+    if ((firstWeekday + date.getDate()) % 7 === 0 || date.getDate() === lastDate.getDate()) {
       console.log();
     } else {
       process.stdout.write(" ");
