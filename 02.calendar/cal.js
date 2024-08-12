9a218f984bfafb58ce1c83339fdec697cfe5f210
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-12 22:43:24 +0900

    firstDataオブジェクトの増加を利用

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -5,14 +5,13 @@ import minimist from "minimist";
 function outputCalendar(year, month) {
   const firstDate = new Date(year, month - 1, 1);
   const lastDate = new Date(year, month, 0);
-  const firstWeekday = firstDate.getDay();
   console.log(`      ${month}月 ${year}`);
   console.log("日 月 火 水 木 金 土");
   process.stdout.write("   ".repeat(firstDate.getDay()));
   while (firstDate <= lastDate) {
     process.stdout.write(firstDate.getDate().toString().padStart(2));
     if (
-      (firstWeekday + firstDate.getDate()) % 7 === 0 ||
+      firstDate.getDay() === 6 ||
       firstDate.getDate() === lastDate.getDate()
     ) {
       console.log();
