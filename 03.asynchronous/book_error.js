9102b5a173086f11c4267b6d54063bc33c2057d6
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-22 20:03:45 +0900

    デフォルトで指定するため削除

--- 03.asynchronous/book_error.js
+++ 03.asynchronous/book_error.js
@@ -3,7 +3,7 @@ import sqlite3 from "sqlite3";
 
 function outputData() {
   const db = new sqlite3.Database(":memory:");
-  const data = fs.readFileSync("books.json", "utf8");
+  const data = fs.readFileSync("books.json");
   const books = JSON.parse(data);
 
   db.run(
@@ -14,7 +14,6 @@ function outputData() {
       );
       let insertCount = 0;
 
-      //jsonデータ2つ目の読み取りで、一位性に反するためエラーを発生させる。insertに失敗した場合、DB上でカラム及びそのidは作成されない。this.lastIDが3になることはない。
       for (let i = 0; i < books.length; i++) {
         insert_statement.run(books[i].title, function (err) {
           if (err) {
