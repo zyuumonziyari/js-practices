e9cca89b97d10c1c134a3319607b10468a7a537f
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-08-22 18:55:55 +0900

    出力の責務を分けるため

---
+++ 03.asynchronous/book_promise.js
@@ -0,0 +1,64 @@
+import fs from "fs";
+import sqlite3 from "sqlite3";
+
+function createTable(db) {
+  return new Promise((resolve) => {
+    db.run(
+      "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+      function () {
+        resolve();
+      },
+    );
+  });
+}
+
+function insertBooks(db, books) {
+  return new Promise((resolve) => {
+    const insert_statement = db.prepare("INSERT INTO books (title) VALUES (?)");
+
+    const promises = books.map((book) => {
+      return new Promise((resolve) => {
+        insert_statement.run(book.title, function () {
+          resolve(this.lastID);
+        });
+      });
+    });
+
+    Promise.all(promises).then((ids) => {
+      insert_statement.finalize(function () {
+        resolve(ids);
+      });
+    });
+  });
+}
+
+function fetchAllBooks(db) {
+  return new Promise((resolve) => {
+    db.all("SELECT title FROM books", (_, rows) => {
+      resolve(rows);
+    });
+  });
+}
+
+function initializeAndExecute() {
+  const data = fs.readFileSync("books.json", "utf8");
+  const books = JSON.parse(data);
+  const db = new sqlite3.Database(":memory:");
+
+  createTable(db)
+    .then(() => insertBooks(db, books))
+    .then((ids) => {
+      ids.forEach((id) => {
+        console.log(`新しく挿入されたレコードのID: ${id}`);
+      });
+      return fetchAllBooks(db);
+    })
+    .then((rows) => {
+      rows.forEach((row) => {
+        console.log(`新しく作成されたレコード値: ${row.title}`);
+      });
+    })
+    .finally(() => db.close());
+}
+
+initializeAndExecute();
