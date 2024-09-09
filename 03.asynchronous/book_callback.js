29040bdd8b2494e76a6878d5c61701125ac1fdc4
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-09-09 16:58:23 +0900

    ファイル名の統一

---
+++ 03.asynchronous/book_callback.js
@@ -0,0 +1,59 @@
+import fs from "fs";
+import sqlite3 from "sqlite3";
+
+export function createTable(db, callback) {
+  db.run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    callback,
+  );
+}
+
+function insertBook(insert_statement, book, callback) {
+  insert_statement.run(book.title, function () {
+    console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
+    callback();
+  });
+}
+
+function fetchAllBooks(db, callback) {
+  db.all("SELECT title FROM books", function (_, rows) {
+    rows.forEach((row) => {
+      console.log(`新しく作成されたレコード値: ${row.title}`);
+    });
+    callback();
+  });
+}
+
+export function closeTable(db, callback) {
+  db.run("DROP TABLE BOOKS", callback);
+}
+
+function initializeAndExecute() {
+  const db = new sqlite3.Database(":memory:");
+  const data = fs.readFileSync("books.json");
+  const books = JSON.parse(data);
+
+  createTable(db, () => {
+    const insert_statement = db.prepare("INSERT INTO books (title) VALUES (?)");
+    let insertCount = 0;
+
+    books.forEach((book) => {
+      insertBook(insert_statement, book, () => {
+        insertCount++;
+        if (insertCount === books.length) {
+          insert_statement.finalize(() => {
+            fetchAllBooks(db, () => {
+              closeTable(db, () => {
+                db.close();
+              });
+            });
+          });
+        }
+      });
+    });
+  });
+}
+
+if (import.meta.url === `file://${process.argv[1]}`) {
+  initializeAndExecute();
+}
