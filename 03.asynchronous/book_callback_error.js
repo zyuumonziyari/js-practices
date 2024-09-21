5bd840496ad379cb6ce0bc291dac90d45d918a71
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-09-21 09:09:44 +0900

    関数を削除

--- 03.asynchronous/book_callback_error.js
+++ 03.asynchronous/book_callback_error.js
@@ -1,49 +1,36 @@
 import fs from "fs";
 import sqlite3 from "sqlite3";
-import { createTable, closeTable } from "./book.js";
-
-function insertBook(insertStatement, book, callback) {
-  insertStatement.run(book.title, function (err) {
-    if (err) {
-      console.error(`データ挿入時にエラーが発生しました: ${err.message}`);
-      callback(err);
-    } else {
-      console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-      callback();
-    }
-  });
-}
-
-function fetchAllBooks(db, callback) {
-  db.all("SELECT author FROM books", function (err, rows) {
-    if (err) {
-      console.error("データ取得時にエラーが発生しました:", err.message);
-      callback(err);
-    } else {
-      rows.forEach((row) => {
-        console.log(`新しく作成されたレコード値: ${row.title}`);
-      });
-      callback();
-    }
-  });
-}
 
 function main() {
   const db = new sqlite3.Database(":memory:");
   const data = fs.readFileSync("books_error.json");
   const books = JSON.parse(data);
 
-  createTable(db, () => {
+  db.run("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", function() {
     const insertStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
     let insertCount = 0;
 
     books.forEach((book) => {
-      insertBook(insertStatement, book, () => {
+      insertStatement.run(book.title, function (err) {
+        if (err) {
+          console.error(`データ挿入時にエラーが発生しました: ${err.message}`);
+        } else {
+          console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
+        }
         insertCount++;
+
         if (insertCount === books.length) {
           insertStatement.finalize(() => {
-            fetchAllBooks(db, () => {
-              closeTable(db, () => {
+            db.all("SELECT author FROM books", function (err, rows) {
+              if (err) {
+                console.error("データ取得時にエラーが発生しました:", err.message);
+              } else {
+                rows.forEach((row) => {
+                  console.log(`新しく作成されたレコード値: ${row.title}`);
+                });
+              }
+
+              db.run("DROP TABLE books", function () {
                 db.close();
               });
             });
