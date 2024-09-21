b07b1a52c07062dde2d479298d7ee8716eccc9e2
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-09-22 00:52:13 +0900

    独自関数の削除

--- 03.asynchronous/book_callback.js
+++ 03.asynchronous/book_callback.js
@@ -1,49 +1,28 @@
-import fs from "fs";
 import sqlite3 from "sqlite3";
-
-export function createTable(db, callback) {
-  db.run(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    callback,
-  );
-}
-
-function insertBook(insertStatement, book, callback) {
-  insertStatement.run(book.title, function () {
-    console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-    callback();
-  });
-}
-
-function fetchAllBooks(db, callback) {
-  db.all("SELECT title FROM books", function (_, rows) {
-    rows.forEach((row) => {
-      console.log(`新しく作成されたレコード値: ${row.title}`);
-    });
-    callback();
-  });
-}
-
-export function closeTable(db, callback) {
-  db.run("DROP TABLE BOOKS", callback);
-}
 
 function main() {
   const db = new sqlite3.Database(":memory:");
-  const data = fs.readFileSync("books.json");
-  const books = JSON.parse(data);
+  const books = [
+    { title: "ピカチュウ" },
+    { title: "カビゴン" },
+    { title: "ヤドン" }
+  ];
 
-  createTable(db, () => {
+  db.run("CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)", function() {
     const insertStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
     let insertCount = 0;
 
     books.forEach((book) => {
-      insertBook(insertStatement, book, () => {
+      insertStatement.run(book.title, function () {
+        console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
         insertCount++;
         if (insertCount === books.length) {
           insertStatement.finalize(() => {
-            fetchAllBooks(db, () => {
-              closeTable(db, () => {
+            db.all("SELECT title FROM books", function (_, rows) {
+              rows.forEach((row) => {
+                console.log(`新しく作成されたレコード値: ${row.title}`);
+              });
+              db.run("DROP TABLE books", function () {
                 db.close();
               });
             });
@@ -53,7 +32,5 @@ function main() {
     });
   });
 }
-
-if (import.meta.url === `file://${process.argv[1]}`) {
-  main();
-}
+            
+main();
