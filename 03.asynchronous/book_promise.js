41c7a3e50895e10cc5e47e3d227a98e8fa69c029
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-11-15 13:12:08 +0900

    sqliteメソッドをPromiseベースで使用できるよう関数でラップ

--- 03.asynchronous/book_promise.js
+++ 03.asynchronous/book_promise.js
@@ -1,78 +1,70 @@
-import fs from "fs";
 import sqlite3 from "sqlite3";
 
-export function createTable(db) {
+function main() {
+  const db = new sqlite3.Database(":memory:");
+  const books = [
+    { title: "ピカチュウ" },
+    { title: "カビゴン" },
+    { title: "ヤドン" },
+  ];
+
   return new Promise((resolve) => {
     db.run(
       "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
       function () {
         resolve();
-      },
+      }
     );
-  });
-}
-
-export function insertBooks(db, books) {
-  return new Promise((resolve) => {
+  })
+  .then(() => {
     const insertStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
-
-    const promises = books.map((book) => {
+    return new Promise((resolve) => {
+      insertStatement.run(books[0].title, function () {
+        console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
+        resolve(this.lastID);
+      });
+    })
+    .then(() => {
       return new Promise((resolve) => {
-        insertStatement.run(book.title, function () {
+        insertStatement.run(books[1].title, function () {
+          console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
           resolve(this.lastID);
         });
       });
-    });
-
-    Promise.all(promises).then((ids) => {
-      insertStatement.finalize(function () {
-        resolve(ids);
-      });
-    });
-  });
-}
-
-export function fetchAllBooks(db) {
-  return new Promise((resolve) => {
-    db.all("SELECT title FROM books", (_, rows) => {
-      resolve(rows);
-    });
-  });
-}
-
-export function closeTable(db) {
-  return new Promise((resolve) => {
-    db.run("DROP TABLE BOOKS", function () {
-      resolve();
-    });
-  });
-}
-
-function main() {
-  const data = fs.readFileSync("books.json");
-  const books = JSON.parse(data);
-  const db = new sqlite3.Database(":memory:");
-
-  createTable(db)
-    .then(() => insertBooks(db, books))
-    .then((ids) => {
-      ids.forEach((id) => {
-        console.log(`新しく挿入されたレコードのid: ${id}`);
-      });
-      return fetchAllBooks(db);
     })
-    .then((rows) => {
-      rows.forEach((row) => {
-        console.log(`新しく作成されたレコード値: ${row.title}`);
+    .then(() => {
+      return new Promise((resolve) => {
+        insertStatement.run(books[2].title, function () {
+          console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
+          resolve(this.lastID);
+        });
       });
     })
-    .finally(() => {
-      closeTable(db).then(() => {
-        db.close();
+    .then(() => {
+      insertStatement.finalize();
+    });
+  })
+  .then(() => {
+    return new Promise((resolve) => {
+      db.all("SELECT title FROM books", (_, rows) => {
+        resolve(rows);
       });
     });
+  })
+  .then((rows) => {
+    rows.forEach((row) => {
+      console.log(`新しく作成されたレコード値: ${row.title}`);
+    });
+  })
+  .then(() => {
+    return new Promise((resolve) => {
+      db.run("DROP TABLE books", function () {
+        resolve();
+      });
+    });
+  })
+  .finally(() => {
+    db.close();
+  });
 }
-
-if (import.meta.url === `file://${process.argv[1]}`) {
-  main();
-}
+main();
