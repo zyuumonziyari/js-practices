fb0861172e92ade69ee5b455edc7e5c589a3302c
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-11-15 13:22:14 +0900

    prettierによる修正

--- 03.asynchronous/book_promise.js
+++ 03.asynchronous/book_promise.js
@@ -13,58 +13,60 @@ function main() {
       "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
       () => {
         resolve();
-      }
+      },
     );
   })
-  .then(() => {
-    const insertStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
-    return new Promise((resolve) => {
-      insertStatement.run(books[0].title, function () {
-        console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-        resolve(this.lastID);
+    .then(() => {
+      const insertStatement = db.prepare(
+        "INSERT INTO books (title) VALUES (?)",
+      );
+      return new Promise((resolve) => {
+        insertStatement.run(books[0].title, function () {
+          console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
+          resolve(this.lastID);
+        });
+      })
+        .then(() => {
+          return new Promise((resolve) => {
+            insertStatement.run(books[1].title, function () {
+              console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
+              resolve(this.lastID);
+            });
+          });
+        })
+        .then(() => {
+          return new Promise((resolve) => {
+            insertStatement.run(books[2].title, function () {
+              console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
+              resolve(this.lastID);
+            });
+          });
+        })
+        .then(() => {
+          insertStatement.finalize();
+        });
+    })
+    .then(() => {
+      return new Promise((resolve) => {
+        db.all("SELECT title FROM books", (_, rows) => {
+          resolve(rows);
+        });
+      });
+    })
+    .then((rows) => {
+      rows.forEach((row) => {
+        console.log(`新しく作成されたレコード値: ${row.title}`);
       });
     })
     .then(() => {
       return new Promise((resolve) => {
-        insertStatement.run(books[1].title, function () {
-          console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-          resolve(this.lastID);
+        db.run("DROP TABLE books", () => {
+          resolve();
         });
       });
     })
-    .then(() => {
-      return new Promise((resolve) => {
-        insertStatement.run(books[2].title, function () {
-          console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-          resolve(this.lastID);
-        });
-      });
-    })
-    .then(() => {
-      insertStatement.finalize();
+    .finally(() => {
+      db.close();
     });
-  })
-  .then(() => {
-    return new Promise((resolve) => {
-      db.all("SELECT title FROM books", (_, rows) => {
-        resolve(rows);
-      });
-    });
-  })
-  .then((rows) => {
-    rows.forEach((row) => {
-      console.log(`新しく作成されたレコード値: ${row.title}`);
-    });
-  })
-  .then(() => {
-    return new Promise((resolve) => {
-      db.run("DROP TABLE books", () => {
-        resolve();
-      });
-    });
-  })
-  .finally(() => {
-    db.close();
-  });
 }
 main();
