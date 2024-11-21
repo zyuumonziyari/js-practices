44d5f06c63186416e773122554297fbe48c6c412
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-11-21 13:17:07 +0900

    insert時のrunをモジュール化

--- 03.asynchronous/book_promise_error.js
+++ 03.asynchronous/book_promise_error.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runPromise } from "./book_utils.js";
+import { runPromise, allPromise, insertStatementPromise } from "./book_utils.js";
 
 function main() {
   const db = new sqlite3.Database(":memory:");
@@ -9,74 +9,32 @@ function main() {
     { title: "ヤドン" },
   ];
 
-  runPromise(db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)")
+  runPromise(db, "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)")
+  .then(() => {
+    const insertStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
+    insertStatementPromise(books[0].title, insertStatement)
     .then(() => {
-      const insertStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
-      
-      return new Promise((resolve) => {
-        insertStatement.run(books[0].title, function (err) {
-          if (err) {
-            console.error(`データ挿入時にエラーが発生しました: ${err.message}`);
-          } else {
-            console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-            resolve(this.lastID);
-          }
-          });
-      })
-        .then(() => {
-          return new Promise((resolve) => {
-            insertStatement.run(books[1].title, function (err) {
-              if (err) {
-                console.error(`データ挿入時にエラーが発生しました: ${err.message}`);
-              } else {
-                console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-                resolve(this.lastID);
-              }
-            });
-          });
-        })
-        .then(() => {
-          return new Promise((resolve) => {
-            insertStatement.run(books[2].title, function (err) {
-              if (err) {
-                console.error(`データ挿入時にエラーが発生しました: ${err.message}`);
-              } else {
-                console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-                resolve(this.lastID);
-              }
-            });
-          });
-        })
-        .then(() => {
-          return new Promise((resolve) => {
-            insertStatement.finalize(() => {
-              resolve();
-            });
-          });
-        });
+      insertStatementPromise(books[1].title, insertStatement)
+    })
+    .then(() => {
+      insertStatementPromise(books[2].title, insertStatement)
     })
     .then(() => {
       return new Promise((resolve) => {
-        db.all("SELECT name FROM books", (err, rows) => {
-          if (err) {
-            console.error(
-              `データ取得時にエラーが発生しました: ${err.message}`,
-            );
-          } else {
-          rows.forEach((row) => {
-            console.log(`新しく作成されたレコード値: ${row.title}`);
-          });
-          resolve(rows);
-        }
+        insertStatement.finalize(() => {
+          resolve();
         });
       });
     })
     .then(() => {
-        runPromise(db, "DROP TABLE books")      
+      return allPromise(db);
+  })
+    .catch((error) => {
+      console.error(`データ取得時にエラーが発生しました: ${error.message}`);
     })
     .finally(() => {
-      db.close();
+      runPromise(db, "DROP TABLE books").finally(() => db.close());
     });
+  })
 }
 main();
