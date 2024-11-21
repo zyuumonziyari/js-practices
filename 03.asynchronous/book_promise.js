1922a3c8d79264312108a6a207b6ed707cb46984
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-11-21 20:35:36 +0900

    prepareを使用しない

--- 03.asynchronous/book_promise.js
+++ 03.asynchronous/book_promise.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runPromise } from "./book_utils.js";
+import { runPromise, insertPromise, allPromise } from "./book_utils.js";
 
 function main() {
   const db = new sqlite3.Database(":memory:");
@@ -9,56 +9,19 @@ function main() {
     { title: "ヤドン" },
   ];
 
-  runPromise(db,
-    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)")
+  runPromise(db, "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)")
     .then(() => {
-      const insertStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
-      
-      return new Promise((resolve) => {
-        insertStatement.run(books[0].title, function () {
-          console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-          resolve(this.lastID);
-        });
-      })
-        .then(() => {
-          return new Promise((resolve) => {
-            insertStatement.run(books[1].title, function () {
-              console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-              resolve(this.lastID);
-            });
-          });
-        })
-        .then(() => {
-          return new Promise((resolve) => {
-            insertStatement.run(books[2].title, function () {
-              console.log(`新しく挿入されたレコードのID: ${this.lastID}`);
-              resolve(this.lastID);
-            });
-          });
-        })
-        .then(() => {
-          return new Promise((resolve) => {
-            insertStatement.finalize(() => {
-              resolve();
-            });
-          });
-        });
+        let bookPromise = Promise.resolve();
+      for (let i = 0; i < books.length; i++) {
+        bookPromise = bookPromise.then(() => insertPromise(db, books[i].title));
+      }
+      return bookPromise;
     })
     .then(() => {
-      return new Promise((resolve) => {
-        db.all("SELECT title FROM books", (_, rows) => {
-          rows.forEach((row) => {
-            console.log(`新しく作成されたレコード値: ${row.title}`);
-          });
-          resolve(rows);
-        });
-      });
-    })
-    .then(() => {
-        runPromise(db, "DROP TABLE books")      
+      return allPromise(db, "SELECT title FROM books");
     })
     .finally(() => {
-      db.close();
+      runPromise(db, "DROP TABLE books").finally(() => db.close());
     });
 }
 main();
--- 03.asynchronous/book_promise_error.js
+++ 03.asynchronous/book_promise_error.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runPromise, allPromise, insertStatementPromise } from "./book_utils.js";
+import { runPromise, insertPromise, allPromise } from "./book_utils.js";
 
 function main() {
   const db = new sqlite3.Database(":memory:");
@@ -10,31 +10,18 @@ function main() {
   ];
 
   runPromise(db, "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)")
-  .then(() => {
-    const insertStatement = db.prepare("INSERT INTO books (title) VALUES (?)");
-    insertStatementPromise(books[0].title, insertStatement)
     .then(() => {
-      insertStatementPromise(books[1].title, insertStatement)
+      const bookPromises = books.map((book) => insertPromise(db, book.title));
+      return Promise.allSettled(bookPromises)
     })
     .then(() => {
-      insertStatementPromise(books[2].title, insertStatement)
+      return allPromise(db, "SELECT name FROM books");
     })
-    .then(() => {
-      return new Promise((resolve) => {
-        insertStatement.finalize(() => {
-          resolve();
-        });
-      });
-    })
-    .then(() => {
-      return allPromise(db);
-  })
     .catch((error) => {
-      console.error(`データ取得時にエラーが発生しました: ${error.message}`);
+      console.error(`エラーが発生しました: ${error.message}`);
     })
     .finally(() => {
       runPromise(db, "DROP TABLE books").finally(() => db.close());
     });
-  })
 }
 main();
