f1e668a17041762ba0b8e2619ec271e94a38041a
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-11-21 20:29:08 +0900

    モジュールの適用

--- 03.asynchronous/book_async.js
+++ 03.asynchronous/book_async.js
@@ -1,28 +1,19 @@
-import fs from "fs";
 import sqlite3 from "sqlite3";
-import {
-  createTable,
-  insertBooks,
-  fetchAllBooks,
-  closeTable,
-} from "./book_promise.js";
+import { runPromise, insertPromise, allPromise } from "./book_utils.js";
 
 async function main() {
-  const data = fs.readFileSync("books.json");
-  const books = JSON.parse(data);
   const db = new sqlite3.Database(":memory:");
+  const books = [
+    { title: "ピカチュウ" },
+    { title: "カビゴン" },
+    { title: "ヤドン" },
+  ];
 
-  await createTable(db);
-  const ids = await insertBooks(db, books);
-  ids.forEach((id) => {
-    console.log(`新しく挿入されたレコードのID: ${id}`);
-  });
-  const rows = await fetchAllBooks(db);
-  rows.forEach((row) => {
-    console.log(`新しく作成されたレコード値: ${row.title}`);
-  });
-  await closeTable(db);
+  await runPromise(db, "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)")
+  const bookPromises = books.map((book) => insertPromise(db, book.title));
+  await Promise.allSettled(bookPromises);
+  await allPromise(db, "SELECT title FROM books");
+  await runPromise(db, "DROP TABLE books");
   db.close();
 }
-
 main();
