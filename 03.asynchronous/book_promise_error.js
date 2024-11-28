b5a25fdbdca29a29eca296b6590011092a61f77b
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-11-28 12:03:21 +0900

    完了を待つ必要のない関数に対して.then()を削除

--- 03.asynchronous/book_promise_error.js
+++ 03.asynchronous/book_promise_error.js
@@ -13,28 +13,18 @@ function main() {
     db,
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   )
-    .then(() => {
-      let bookPromise = Promise.resolve();
-      for (const book of books) {
-        bookPromise = bookPromise.then(() =>
-          runPromise(db, "INSERT INTO books (title) VALUES (?)", book.title),
-        );
-      }
-      return bookPromise;
-    })
-    .catch((insertError) => {
-      console.error(insertError.message);
-    })
-    .then(() => {
-      return allPromise(db, "SELECT name FROM books");
-    })
-    .catch((selectError) => {
-      console.error(selectError.message);
-    })
-    .then(() => {
-      runPromise(db, "DROP TABLE books");
-    })
-    .finally(() => db.close());
+  .then(() => runPromise(db, "INSERT INTO books (title) VALUES (?)", books[0].title))
+  .then(() => runPromise(db, "INSERT INTO books (title) VALUES (?)", books[1].title))
+  .then(() => runPromise(db, "INSERT INTO books (title) VALUES (?)", books[2].title))
+  .catch((insertError) => {
+    console.error(insertError.message);
+    return allPromise(db, "SELECT name FROM books");
+  })
+  .catch((selectError) => {
+    console.error(selectError.message);
+    return runPromise(db, "DROP TABLE books");
+  })
+  .finally(() => db.close());
 }
 
 main();
