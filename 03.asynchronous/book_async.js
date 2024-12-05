3ccfc863cf1fc6afc3a87ff04d8eebf808a50a80
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-12-05 22:53:07 +0900

    db操作に関わる関数のモジュールと分かるようにファイル名を修正

--- 03.asynchronous/book_async.js
+++ 03.asynchronous/book_async.js
@@ -1,5 +1,5 @@
 import sqlite3 from "sqlite3";
-import { runPromise, allPromise, closePromise } from "./book_utils.js";
+import { runPromise, allPromise, closePromise } from "./db_utils.js";
 
 async function main() {
   const db = new sqlite3.Database(":memory:");
