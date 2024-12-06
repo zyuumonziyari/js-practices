f80faeed4c23396a9b71bc2dd63d8a12cf28f1d8
zyuumonziyari <sou0921@fuji.waseda.jp>
2024-12-06 20:50:33 +0900

    finallyが全体に対するものとなるように修正、DROP TABLEの処理が例外処理の対象とならないように修正

--- 03.asynchronous/book_async_error.js
+++ 03.asynchronous/book_async_error.js
@@ -14,26 +14,28 @@ async function main() {
     "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   );
   try {
-    for (const book of books) {
-      const result = await runPromise(
-        db,
-        "INSERT INTO books (title) VALUES (?)",
-        book.title,
-      );
-      console.log(`新しく挿入されたレコードのID: ${result.bookId}`);
+    try {
+      for (const book of books) {
+        const result = await runPromise(
+          db,
+          "INSERT INTO books (title) VALUES (?)",
+          book.title,
+        );
+        console.log(`新しく挿入されたレコードのID: ${result.bookId}`);
+      }
+    } catch (err) {
+      console.error(`データ挿入時にエラーが発生しました: ${err.message}`);
     }
-  } catch (err) {
-    console.error(`データ挿入時にエラーが発生しました: ${err.message}`);
-  }
-  try {
-    const rows = await allPromise(db, "SELECT name FROM books");
-    rows.forEach((row) => {
-      console.log(`新しく作成されたレコード値: ${row.title}`);
-    });
-  } catch (err) {
-    console.error(`データ取得時にエラーが発生しました: ${err.message}`);
-    await runPromise(db, "DROP TABLE books");
+    try {
+      const rows = await allPromise(db, "SELECT name FROM books");
+      rows.forEach((row) => {
+        console.log(`新しく作成されたレコード値: ${row.title}`);
+      });
+    } catch (err) {
+      console.error(`データ取得時にエラーが発生しました: ${err.message}`);
+    }
   } finally {
+    await runPromise(db, "DROP TABLE books");
     await closePromise(db);
   }
 }
